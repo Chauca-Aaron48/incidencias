2796f988bfbbce50d11bed1f18f83b5ad74b3f1b
Nestor-Sarmiento <nesdav1005@gmail.com>
2026-04-21 19:10:22 -0500

    Corregir id, añadir legend y redirigir al listar si ya tiene sesión activa

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
+<?php
+session_start();
+if (isset($_SESSION['nombre'])) {
+    header("Location: listar.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistema de Incidencias Técnicas</title>
 </head>
 <body>
     <h1>Sistemas de Incidencias Técnicas</h1>
     <h2>Bienvenido, ingrese sus credenciales por favor</h2>
     <form action="acceso.php" method="post">
         <fieldset>
+            <legend>Inicio de Sesión</legend>
             <label for="nombre">Usuario:</label><br>
             <input type="text" name="nombre" id="nombre" placeholder="Ingrese su usuario" required>
             <br>
-            <label for="password">Contraseña:</label><br>
-            <input type="password" name="clave" id="password" placeholder="Ingrese su contraseña" required>
+            <label for="clave">Contraseña:</label><br>
+            <input type="password" name="clave" id="clave" placeholder="Ingrese su contraseña" required>
             <br><br>
             <input type="submit" value="Ingresar">
         </fieldset>
